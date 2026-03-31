3d56b67c990f54c73f48dbe9853f99aafbab8cf6
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2026-03-31 18:48:56 +0200

    Add implementation of ft_write function in ft_write.s

---
+++ ft_write.s
@@ -0,0 +1,31 @@
+;
+; **************************************************************************** ;
+;                                                                              ;
+;                                                         :::      ::::::::    ;
+;    ft_strlen.s                                        :+:      :+:    :+:    ;
+;                                                     +:+ +:+         +:+      ;
+;    By: volmer <volmer@student.42.fr>              +#+  +:+       +#+         ;
+;                                                 +#+#+#+#+#+   +#+            ;
+;    Created: 2026/03/25 13:10:56 by volmer            #+#    #+#              ;
+;    Updated: 2026/03/25 15:03:21 by volmer           ###   ########.fr        ;
+;                                                                              ;
+; **************************************************************************** ;
+
+section .text
+	global _ft_write
+
+_ft_write:
+	; rdi = fd, rsi = buf, rdx = count
+	mov rax, 0x2000004 ; numero syscall para sys_write en macOS
+	cmp rdi, 0; si el fd es -1, no es válido, devuelve -1
+	jl error ; jl = jump if less, salta a error si rdi < 0
+	syscall ; realiza la llamada al sistema, los argumentos ya están en rdi, rsi y rdx
+	jc error ; jc = jump if carry, si la syscall devuelve un error, el flag de carry se establece, salta a error
+	jge done ; si rax >= 0, salta a done jgc = jump if greater, salta a done si rax > 0
+
+error:
+	mov rax, -1 ; devuelve -1 en caso de error
+	jmp done ; salta a done para retornar el valor en rax
+
+done:
+	ret ; retorna el número de bytes escritos o -1 en caso de error
