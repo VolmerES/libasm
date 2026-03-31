629559e87bea81ed1dffac534959f7f2c94d2a4e
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2026-03-31 22:09:30 +0200

    Fix extern declaration and streamline error handling in ft_write

--- ft_write.s
+++ ft_write.s
@@ -13,7 +13,7 @@
 
 section .text
 	global _ft_write
-	extern __error ; Declaración de la función de error, asumiendo que existe en otro archivo
+	extern ___error ; Declaración de la función de error, asumiendo que existe en otro archivo
 
 _ft_write:
 	; rdi = fd, rsi = buf, rdx = count
@@ -24,16 +24,13 @@ _ft_write:
 
 	syscall ; realiza la llamada al sistema, los argumentos ya están en rdi, rsi y rdx
 	jc error ; jc = jump if carry, si la syscall devuelve un error, el flag de carry se establece, salta a error
-	jge done ; si rax >= 0, salta a done jgc = jump if greater, salta a done si rax > 0
+	ret
 
 error:
 	mov r8, rax ; devuelve -1 en caso de error
 	sub rsp, 8 ; reserva espacio en la pila para el valor de retorno
-	call __error ; llama a la función de error para imprimir el mensaje
+	call ___error ; llama a la función de error para imprimir el mensaje
 	add rsp, 8 ; limpia la pila después de la llamada a error
-	mov dword [rsp], r8d ; coloca el valor de retorno (-1) en la pila para devolverlo
+	mov dword [rax], r8d ; coloca el valor de retorno (-1) en la pila para devolverlo
 	mov rax, -1 ; devuelve -1 en caso de error
-	jmp done ; salta a done para retornar el valor en rax
-
-done:
-	ret ; retorna el número de bytes escritos o -1 en caso de error
+	ret
